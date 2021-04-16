ddb1280d6d85f9696832f5fddd9b6c8626214cb6
JosephLilfish <apatryk19@gmail.com>
2021-04-16 10:18:40 +0000

    headers

--- header.php
+++ header.php
@@ -230,46 +230,10 @@
 
 				</div> <!-- #et-secondary-menu -->
 
-                    <div class="search_in_header">
-<?php if ( function_exists( 'aws_get_search_form' ) ) { aws_get_search_form(); } ?>
-                        <!-- <form role="search" method="get" class="et-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-
-                            <?php
-
-                            // printf( '<input type="search" class="et-search-field" placeholder="" value="%2$s" name="s" title="%3$s" />',
-
-                            // esc_attr__( 'Search &hellip;', 'Divi' ),
-
-                            // get_search_query(),
-
-                            // esc_attr__( 'Search for:', 'Divi' )
-
-                            // );
-
-
-
-                            /**
-
-                            * Fires inside the search form element, just before its closing tag.
-
-                            *
-
-                            // * @since ??
-
-                            // */
-
-                            // do_action( 'et_search_form_fields' );
-
-                            ?>
-
-                            <button type="submit" id="searchsubmit_header"><img class="on_hover" src="<?php echo get_stylesheet_directory_uri();?>/gfx/ionic-ios-search.png" alt="search"/> Szukaj</button>
-
-                        </form> -->
-
-                    </div>
-
+				<div class="topBarInfo">Darmowa dostawa od <span>300zł</span></div>
+			
 			</div> <!-- .container -->
-
+			<hr>
             
 
 		</div> <!-- #top-header -->
--- includes/main-slider.php
+++ includes/main-slider.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 function iguana_main_slider( ) {
 
@@ -7,13 +6,3 @@ function iguana_main_slider( ) {
     return ob_get_clean();
 }
 add_shortcode( 'iguana_main_slider', 'iguana_main_slider' );
-=======
-<?php
-function iguana_main_slider( ) {
-
-    ob_start();
-    include_once( 'partials/main-slider.php' );
-    return ob_get_clean();
-}
-add_shortcode( 'iguana_main_slider', 'iguana_main_slider' );
->>>>>>> d7a2223e709ae53b4626bf2012b4732d46b28b6e
